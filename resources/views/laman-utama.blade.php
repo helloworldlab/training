dd9bc44678e922cc904a8287295b883119e96c9b
Khai Rahman <khairahman@helloworldlab.com>
2021-02-25 10:46:38 +0800

    Tambah border bawah pada Navbar

--- resources/views/laman-utama.blade.php
+++ resources/views/laman-utama.blade.php
@@ -7,7 +7,7 @@
         <title>Training</title>
     </head>
     <body>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
             <div class="container">
                 <a class="navbar-brand" href="#">{{ config('app.name') }}</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
