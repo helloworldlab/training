ec6311b3d474b0dd8946385798e834e66d60a74e
Khai Rahman <khairahman@helloworldlab.com>
2021-02-26 09:54:35 +0800

    Tambah UI untuk errors

--- resources/views/pengguna/create.blade.php
+++ resources/views/pengguna/create.blade.php
@@ -27,13 +27,23 @@
                 <div class="col-12 col-md-6">
                     <div class="form-group">
                         <label class="font-weight-bold">Nama&nbsp;<span class="text-danger">*</span></label>
-                        <input class="form-control" type="text" name="nama" value="{{ old('nama') }}" autofocus>
+                        <input class="form-control @error('nama') is-invalid @enderror" type="text" name="nama" value="{{ old('nama') }}" autofocus>
+                        @error('nama')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                 </div>
                 <div class="col-12 col-md-6">
                     <div class="form-group">
                         <label class="font-weight-bold">No. Kad Pengenalan&nbsp;<span class="text-danger">*</span></label>
-                        <input class="form-control" type="text" name="no_kad_pengenalan" value="{{ old('no_kad_pengenalan') }}">
+                        <input class="form-control @error('no_kad_pengenalan') is-invalid @enderror" type="text" name="no_kad_pengenalan" value="{{ old('no_kad_pengenalan') }}">
+                        @error('no_kad_pengenalan')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                 </div>
             </div>
@@ -42,23 +52,33 @@
                 <div class="col-12 col-md-6">
                     <div class="form-group">
                         <label class="font-weight-bold">Jantina&nbsp;<span class="text-danger">*</span></label>
-                        <select name="jantina" class="form-control">
+                        <select name="jantina" class="form-control @error('jantina') is-invalid @enderror">
                             <option disabled selected>Sila Pilih</option>
                             @foreach ($senaraiJantina as $jantina)
                                 <option value="{{ $jantina->id }}">{{ $jantina->nama }}</option>
                             @endforeach
                         </select>
+                        @error('jantina')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                 </div>
                 <div class="col-12 col-md-6">
                     <div class="form-group">
                         <label class="font-weight-bold">Agensi&nbsp;<span class="text-danger">*</span></label>
-                        <select name="agensi" class="form-control">
+                        <select name="agensi" class="form-control @error('agensi') is-invalid @enderror">
                             <option disabled selected>Sila Pilih</option>
                             @foreach ($senaraiAgensi as $agensi)
                                 <option value="{{ $agensi->id }}">{{ $agensi->nama }}</option>
                             @endforeach
                         </select>
+                        @error('agensi')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                 </div>
             </div>
@@ -67,13 +87,23 @@
                 <div class="col-12 col-md-6">
                     <div class="form-group">
                         <label class="font-weight-bold">Emel&nbsp;<span class="text-danger">*</span></label>
-                        <input class="form-control" type="email" name="emel" value="{{ old('emel') }}">
+                        <input class="form-control @error('emel') is-invalid @enderror" type="email" name="emel" value="{{ old('emel') }}">
+                        @error('emel')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                 </div>
                 <div class="col-12 col-md-6">
                     <div class="form-group">
                         <label class="font-weight-bold">No. Telefon&nbsp;<span class="text-danger">*</span></label>
-                        <input class="form-control" type="text" name="no_telefon" value="{{ old('no_telefon') }}">
+                        <input class="form-control @error('no_telefon') is-invalid @enderror" type="text" name="no_telefon" value="{{ old('no_telefon') }}">
+                        @error('no_telefon')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                 </div>
             </div>
@@ -100,12 +130,12 @@
 
             selectJantina.selectpicker({
                 liveSearch: true,
-                styleBase: 'form-control border bg-white'
+                styleBase: 'form-control border bg-white @error('jantina') border-danger @enderror'
             });
 
             selectAgensi.selectpicker({
                 liveSearch: true,
-                styleBase: 'form-control border bg-white'
+                styleBase: 'form-control border bg-white @error('agensi') border-danger @enderror'
             });
         });
     </script>
